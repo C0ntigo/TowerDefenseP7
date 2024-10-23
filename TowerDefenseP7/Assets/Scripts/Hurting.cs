1db09978b0b782a660313f18051ec56373935dd8
Xavier Real <720838@student.dusd.net>
2024-10-23 14:22:47 -0700

    doing stuff like UI
    next time remeber to push to origin

---
+++ TowerDefenseP7/Assets/Scripts/Hurting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurting : MonoBehaviour
+{
+    public TextMeshPro livesText;
+    private int lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+    }
+    public void Start
+}
